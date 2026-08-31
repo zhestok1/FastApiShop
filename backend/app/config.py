1f289bff8de077b589347fc2279b8ae3f9f1e93d
zhestok1 <rp3truhin@yandex.ru>
2026-08-31 09:15:39 +0400

    add cfg and database settings

---
+++ backend/app/config.py
@@ -0,0 +1,22 @@
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    
+    app_name: str = 'FastAPI Shop'
+    debug: bool = True 
+    database_url: str = "sqlite:///./shop.db"
+    cors_origins: list = [
+        'http://localhost:5173',
+        'http://localhost:3000',
+        'http://127.0.0.1:5173',
+        'http://localhost:3000',
+    ]
+    
+    static_dir: str = 'static'
+    images_dir: str = 'static/images'
+    
+    class Config:
+        env_file = '.env'
+        
+settings = Settings()
+        
---
+++ backend/app/database.py
@@ -0,0 +1,22 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from .config import settings 
+
+engine = create_engine(
+    settings.database_url,
+)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db 
+    finally:
+        db.close()
+        
+def init_db():
+    Base.metadata.create_all(bind=engine)
+    
